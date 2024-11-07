49a4d4233b272b3f82e90df3cbfa529f37b5e58e
Guilherme Davanso <gui.social.ti@gmail.com>
2024-11-06 21:35:56 -0300

    feat: ensure transactions table labels are rendered properly

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { TransactionColumns } from "./_colums";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUpIcon } from "lucide-react";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
@@ -9,6 +11,10 @@ const TransactionsPage = async () => {
       {/* TÍTULO E BOTÃO */}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full font-bold">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
       </div>
       <DataTable columns={TransactionColumns} data={transactions} />
     </div>
