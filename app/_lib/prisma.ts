95cc64227a683a25d0a04f285952a9e057907c89
Guilherme Davanso <gui.social.ti@gmail.com>
2024-11-06 21:08:38 -0300

    feat: add transaction type badge

---
+++ app/_lib/prisma.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+/* Basicamente, uma logica que nao deixa eu abrir mais
+de uma conexao com meu banco se eu nao estiver em producao*/
+
+export const db = prisma;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,18 @@
-const TransactionsPage = () => {
-  return <h1>Transaction pages</h1>;
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_colums";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+      </div>
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
